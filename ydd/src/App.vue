899a9bb34a1b31162f13b89951de3dc0c7df80fd
mumuyu65 <571014594@qq.com>
2017-06-05 00:25:58 +0800

    new vue demo

---
+++ ydd/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div class="content">
+      <ul>
+        <li><router-link to="/">首页</router-link></li>
+        <li><router-link to="/todolist">todolist</router-link></li>
+      </ul>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+  .content ul{
+    list-styled:none;
+  }
+
+.content ul li{
+  display:inline-block;
+  margin-right:20px;
+}
+</style>
